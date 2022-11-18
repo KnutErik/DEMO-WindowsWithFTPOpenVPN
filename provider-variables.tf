fb9738c2a21ac9e57a337cfc566b4ff5fe503a44
KnutErik <93346498+KnutErik@users.noreply.github.com>
2022-11-18 13:51:19 +0100

    Update provider-variables.tf

--- provider-variables.tf
+++ provider-variables.tf
@@ -27,12 +27,12 @@
 
 # Define Terraform provider
 terraform {
- required_version = ">= 0.12"
+ required_version = ">= 0.13"
 }
 #Configure the Azure Provider
 provider "azurerm" {
   features {}
-  version         = ">= 2.0"
+  # version         = ">= 2.0"
   environment     = "public"
  # subscription_id = var.azure_subscription_id
  # client_id       = var.azure_client_id
